31c7f90bb8af1e5909d6a211800ad2aee464a2a7
James Trew <j.trew10@gmail.com>
2021-07-13 16:53:30 -0400

    initial

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- load all plugins
+require "pluginList"
+require "options"
+
+local g = vim.g
+
+g.mapleader = " "
+g.auto_save = false
+
+-- colorscheme related stuff
+
+g.nvchad_theme = "onedark"
+local base16 = require "base16"
+base16(base16.themes["onedark"], true)
+
+require "highlights"
+require "mappings"
+
+require("utils").hideStuff()
---
+++ lua/pluginList.lua
@@ -0,0 +1,171 @@
+local packer = require("packer")
+local use = packer.use
+
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float {border = "single"}
+        end
+    },
+    git = {
+        clone_timeout = 600 -- Timeout, in seconds, for git clones
+    }
+}
+
+return packer.startup(
+    function()
+        use "wbthomason/packer.nvim"
+
+        use {
+            "glepnir/galaxyline.nvim",
+            config = function()
+                require("plugins.statusline").config()
+            end
+        }
+
+        -- color related stuff
+        use "siduck76/nvim-base16.lua"
+
+        use {
+            "norcalli/nvim-colorizer.lua",
+            event = "BufRead",
+            config = function()
+                require("colorizer").setup()
+                vim.cmd("ColorizerReloadAllBuffers")
+            end
+        }
+
+        -- language related plugins
+        use {
+            "nvim-treesitter/nvim-treesitter",
+            event = "BufRead",
+            config = function()
+                require("plugins.treesitter").config()
+            end
+        }
+
+        use {
+            "kabouzeid/nvim-lspinstall",
+            event = "BufRead"
+        }
+
+        use {
+            "neovim/nvim-lspconfig",
+            after = "nvim-lspinstall",
+            config = function()
+                require("plugins.lspconfig").config()
+            end
+        }
+
+        use {
+            "onsails/lspkind-nvim",
+            event = "BufRead",
+            config = function()
+                require("lspkind").init()
+            end
+        }
+
+        -- load compe in insert mode only
+        use {
+            "hrsh7th/nvim-compe",
+            event = "InsertEnter",
+            config = function()
+                require("plugins.compe").config()
+            end,
+            wants = {"LuaSnip"},
+            requires = {
+                {
+                    "L3MON4D3/LuaSnip",
+                    wants = "friendly-snippets",
+                    event = "InsertCharPre",
+                    config = function()
+                        require("plugins.compe").snippets()
+                    end
+                },
+                {
+                    "rafamadriz/friendly-snippets",
+                    event = "InsertCharPre"
+                }
+            }
+        }
+
+        use {"sbdchd/neoformat", cmd = "Neoformat"}
+
+        -- file managing , picker etc
+        use {
+            "kyazdani42/nvim-tree.lua",
+            cmd = "NvimTreeToggle",
+            config = function()
+                require("plugins.nvimtree").config()
+            end
+        }
+
+        use {
+            "kyazdani42/nvim-web-devicons",
+            config = function()
+                require("plugins.icons").config()
+            end
+        }
+
+        use {
+            "nvim-telescope/telescope.nvim",
+            requires = {
+                {"nvim-lua/popup.nvim"},
+                {"nvim-lua/plenary.nvim"}
+            },
+            cmd = "Telescope",
+            config = function()
+                require("plugins.telescope").config()
+            end
+        }
+
+        use {"nvim-telescope/telescope-fzf-native.nvim", run = "make", cmd = "Telescope"}
+
+        use {
+            "nvim-telescope/telescope-media-files.nvim",
+            cmd = "Telescope"
+        }
+
+        -- git stuff
+        use {
+            "lewis6991/gitsigns.nvim",
+            event = "BufRead",
+            config = function()
+                require("plugins.gitsigns").config()
+            end
+        }
+
+        -- misc plugins
+        use {
+            "windwp/nvim-autopairs",
+            after = "nvim-compe",
+            config = function()
+                require("nvim-autopairs").setup()
+                require("nvim-autopairs.completion.compe").setup(
+                    {
+                        map_cr = true,
+                        map_complete = true -- insert () func completion
+                    }
+                )
+            end
+        }
+
+        use {"andymass/vim-matchup", event = "CursorMoved"}
+
+        use {
+            "terrortylor/nvim-comment",
+            cmd = "CommentToggle",
+            config = function()
+                require("nvim_comment").setup()
+            end
+        }
+
+        use {
+            "lukas-reineke/indent-blankline.nvim",
+            event = "BufRead",
+            setup = function()
+                require("utils").blankline()
+            end
+        }
+    end
+)
