52976c0d6908798d4a49900f69e83d35379dcdad
James Trew <j.trew10@gmail.com>
2021-08-28 21:32:04 -0400

    clean up telescope settings

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,80 +1,17 @@
 local utils = require("utils")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
-local sorters = require("telescope.sorters")
 local themes = require("telescope.themes")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local config = require("telescope.config")
-local Path = require("plenary.path")
+
+local tele_utils = require("setup.telescope.tele_utils")
 
 local M = {}
 
 M.config = function()
-  require("telescope").setup({
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case",
-      },
-      prompt_prefix = "  ",
-      selection_caret = " ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "descending",
-      layout_strategy = "horizontal",
-      layout_config = {
-        horizontal = {
-          prompt_position = "bottom",
-          preview_width = 0.55,
-          results_width = 0.8,
-        },
-        vertical = {
-          mirror = false,
-        },
-        width = 0.87,
-        height = 0.80,
-        preview_cutoff = 120,
-      },
-      file_sorter = sorters.get_fuzzy_file,
-      file_ignore_patterns = { "node_modules" },
-      generic_sorter = sorters.get_generic_fuzzy_sorter,
-      path_display = { "smart" },
-      winblend = 0,
-      border = {},
-      borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-      color_devicons = true,
-      use_less = true,
-      set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-      file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-      grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-      qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
-      -- Developer configurations: Not meant for general override
-      buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-    },
-    extensions = {
-      fzf = {
-        fuzzy = true, -- false will only do exact matching
-        override_generic_sorter = false, -- override the generic sorter
-        override_file_sorter = true, -- override the file sorter
-        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-        -- the default case_mode is "smart_case"
-      },
-      media_files = {
-        filetypes = { "png", "webp", "jpg", "jpeg" },
-        find_cmd = "rg", -- find command (defaults to `fd`)
-      },
-      frecency = {
-        ignore_patterns = { "*.git/*", "*/tmp/*", "*/node_modules/*" },
-      },
-    },
-  })
+  require("telescope").setup(require("setup.telescope.defaults"))
 
   require("telescope").load_extension("fzf")
   require("telescope").load_extension("media_files")
@@ -82,50 +19,6 @@ M.config = function()
   require("telescope").load_extension("project")
   require("telescope").load_extension("neoclip")
   -- require("telescope").load_extension "frecency"
-end
-
-local delete_file = function(prompt_bufnr)
-  local fpath = action_state.get_selected_entry().value
-  local ans = vim.fn.input("Are you sure you want to remove " .. fpath .. "? y/[N] ")
-  utils.clear_prompt()
-  if ans ~= "y" then
-    return
-  end
-
-  if utils.is_dir(fpath) then
-    Path:new(fpath):rmdir()
-  else
-    Path:new(fpath):rm()
-  end
-  print(fpath .. " successfully removed")
-
-  actions.close(prompt_bufnr)
-end
-
-local rename_file = function()
-  local fpath = action_state.get_selected_entry().value
-  local new_name = vim.fn.input("Rename ", fpath)
-  utils.clear_prompt()
-  Path:new(fpath):rename({ new_name = new_name })
-end
-
-local create_file = function(prompt_bufnr)
-  local fpath = action_state.get_selected_entry().value
-  local new_file = vim.fn.input("Create file: ", fpath .. "/")
-  utils.clear_prompt()
-
-  if not utils.is_dir(new_file) then
-    actions.close(prompt_bufnr)
-    Path:new(new_file):touch({ parents = true })
-    vim.cmd(string.format(":e %s", new_file))
-  else
-    print("Given path not a valid file name")
-  end
-end
-
-local yank_fpath = function()
-  local entry = action_state.get_selected_entry()
-  vim.fn.setreg("+", entry.value)
 end
 
 M.search_dotfiles = function()
@@ -143,10 +36,10 @@ M.find_files = function(opts)
   opts.attach_mappings = function(_, map)
     map("i", "<C-n>", actions.move_selection_previous)
     map("i", "<C-p>", actions.move_selection_next)
-    map("i", "<C-y>d", delete_file)
-    map("i", "<C-y>r", rename_file)
-    map("i", "<C-y>y", yank_fpath)
-    map("n", "yy", yank_fpath)
+    map("i", "<C-y>d", tele_utils.delete_file)
+    map("i", "<C-y>r", tele_utils.rename_file)
+    map("i", "<C-y>y", tele_utils.yank_fpath)
+    map("n", "yy", tele_utils.yank_fpath)
 
     return true
   end
@@ -173,7 +66,7 @@ M.find_dir = function()
 
   opts.entry_maker = require("telescope.make_entry").gen_from_file(opts)
   opts.attach_mappings = function(prompt_bufnr, map)
-    map("i", "<C-y>c", create_file)
+    map("i", "<C-y>c", tele_utils.create_file)
     map("i", "<C-h>", function()
       actions.close(prompt_bufnr)
       vim.cmd(":Ntree")
@@ -255,10 +148,10 @@ end
 M.file_browser = function()
   require("telescope.builtin").file_browser({
     attach_mappings = function(_, map)
-      map("i", "<C-y>d", delete_file)
-      map("i", "<C-y>r", rename_file)
-      map("i", "<C-y>y", yank_fpath)
-      map("n", "yy", yank_fpath)
+      map("i", "<C-y>d", tele_utils.delete_file)
+      map("i", "<C-y>r", tele_utils.rename_file)
+      map("i", "<C-y>y", tele_utils.yank_fpath)
+      map("n", "yy", tele_utils.yank_fpath)
 
       return true
     end,
---
+++ lua/setup/telescope/tele_utils.lua
@@ -0,0 +1,52 @@
+local utils = require("utils")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+local Path = require("plenary.path")
+
+local M = {}
+
+M.delete_file = function(prompt_bufnr)
+  local fpath = action_state.get_selected_entry().value
+  local ans = vim.fn.input("Are you sure you want to remove " .. fpath .. "? y/[N] ")
+  utils.clear_prompt()
+  if ans ~= "y" then
+    return
+  end
+
+  if utils.is_dir(fpath) then
+    Path:new(fpath):rmdir()
+  else
+    Path:new(fpath):rm()
+  end
+  print(fpath .. " successfully removed")
+
+  actions.close(prompt_bufnr)
+end
+
+M.rename_file = function()
+  local fpath = action_state.get_selected_entry().value
+  local new_name = vim.fn.input("Rename ", fpath)
+  utils.clear_prompt()
+  Path:new(fpath):rename({ new_name = new_name })
+end
+
+M.create_file = function(prompt_bufnr)
+  local fpath = action_state.get_selected_entry().value
+  local new_file = vim.fn.input("Create file: ", fpath .. "/")
+  utils.clear_prompt()
+
+  if not utils.is_dir(new_file) then
+    actions.close(prompt_bufnr)
+    Path:new(new_file):touch({ parents = true })
+    vim.cmd(string.format(":e %s", new_file))
+  else
+    print("Given path not a valid file name")
+  end
+end
+
+M.yank_fpath = function()
+  local entry = action_state.get_selected_entry()
+  vim.fn.setreg("+", entry.value)
+end
+
+return M
