939bed90110bbfeba36bb9b265a854479ceacd26
James Trew <j.trew10@gmail.com>
2021-09-24 23:26:44 -0400

    disable insert mode on start for terminal

--- lua/options.lua
+++ lua/options.lua
@@ -76,8 +76,6 @@ vim.cmd([[autocmd BufEnter * setlocal formatoptions-=r formatoptions-=o]])
 
 vim.cmd("autocmd TextYankPost * silent! lua vim.highlight.on_yank {}")
 
-vim.cmd("autocmd TermOpen * startinsert")
-
 -- autoformat on save
 vim.cmd([[
     augroup fmt
