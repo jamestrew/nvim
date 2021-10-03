07fdae8d253bc527afb1302212d0eb55476bb4a5
James Trew <j.trew10@gmail.com>
2021-10-03 19:22:54 -0400

    neogit settings

--- lua/setup/neogit.lua
+++ lua/setup/neogit.lua
@@ -4,7 +4,8 @@ M.config = function()
   local neogit = require("neogit")
 
   neogit.setup({
-    disable_insert_on_commit = false,
+    disable_insert_on_commit = true,
+    disable_commit_confirmation = true,
     commit_popup = {
       kind = "split",
     },
