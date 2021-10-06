ce662c21b72b036a2a01996288d49b1347d6db89
James Trew <j.trew10@gmail.com>
2021-10-05 21:45:52 -0400

    telescope and jsx highlighting correction #21

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -16,7 +16,6 @@ M.config = function()
   require("neoclip").setup(defaults.neoclip)
 
   require("telescope").load_extension("fzf")
-  require("telescope").load_extension("media_files")
   require("telescope").load_extension("git_worktree")
   require("telescope").load_extension("neoclip")
   -- require("telescope").load_extension "frecency"
--- test/test.jsx
+++ test/test.jsx
@@ -1,2 +1,15 @@
-const a = { foo: 'bar' }
-console.log(a.foo)
+import React, { useState } from "react";
+const a = { foo: "bar" };
+console.log(a.foo);
+
+export const foo = () => {
+  const [bar, setBar] = useState("baz");
+
+  return (
+    <>
+      <div>
+        <p>FOO BAR!</p>
+      </div>
+    </>
+  );
+};
