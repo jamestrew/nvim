6c0dd1488c19337288dff5b79f83803215aa7617
James Trew <j.trew10@gmail.com>
2021-10-11 16:08:35 -0400

    squash! clean up

--- lua/setup/worktree.lua
+++ lua/setup/worktree.lua
@@ -4,10 +4,7 @@ M.config = function()
   local worktree = require("git-worktree")
   local harpoon = require("harpoon")
 
-  worktree.on_tree_change(function(op, path, upstream)
-    os.execute("sleep" .. tonumber(5))
-    print("WORKTREE", op, path, upstream)
-
+  worktree.on_tree_change(function(op, _, _)
     if op == worktree.Operations.Switch then
       harpoon.mark.clear_all()
       harpoon.term.clear_all()
