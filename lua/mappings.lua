4457f8d0619adabe5018675c1a24fe1f8e24bb28
James Trew <j.trew10@gmail.com>
2021-12-10 14:05:05 -0500

    tweak & clean up mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,6 @@
 local utils = require("utils")
+local nnoremap = utils.nnoremap
+local vnoremap = utils.vnoremap
 local silent = { silent = true }
 local sexpr = { silent = true, expr = true }
 
@@ -8,100 +10,101 @@ vim.cmd([[command Wq :wq]])
 
 -- move line(s) up/down
 -- BUG: ESC followed quickly by j/k triggers the next two commands
--- utils.nnoremap("<A-j>", ":m .+1<CR>==")
--- utils.nnoremap("<A-k>", ":m .-2<CR>==")
+-- nnoremap("<A-j>", ":m .+1<CR>==")
+-- nnoremap("<A-k>", ":m .-2<CR>==")
 -- utils.imap("<A-j>", "<Esc>:m .+1<CR>==i")
 -- utils.imap("<A-k>", "<Esc>:m .-2<CR>==i")
-utils.vnoremap("<A-j>", ":m '>+1<CR>gv=gv", silent)
-utils.vnoremap("<A-k>", ":m '<-2<CR>gv=gv", silent)
+vnoremap("<A-j>", ":m '>+1<CR>gv=gv", silent)
+vnoremap("<A-k>", ":m '<-2<CR>gv=gv", silent)
 
 -- paste/delete and keep register clean
-utils.vnoremap("<leader>p", '"_dP')
-utils.vnoremap("<leader>d", '"_d')
+vnoremap("<leader>p", '"_dP')
+vnoremap("<leader>d", '"_d')
 
 -- better indenting
-utils.vnoremap("<", "<gv", silent)
-utils.vnoremap(">", ">gv", silent)
+vnoremap("<", "<gv", silent)
+vnoremap(">", ">gv", silent)
 
 -- easier exit insert mode in the terminal
 utils.tnoremap("<Esc>", "<C-\\><C-n>")
 
 -- keeps jumps centered
-utils.nnoremap("n", "nzzzv")
-utils.nnoremap("N", "Nzzzv")
-utils.nnoremap("<leader>J", "mzJ`z")
+nnoremap("n", "nzzzv")
+nnoremap("N", "Nzzzv")
+nnoremap("<leader>J", "mzJ`z")
 
-utils.nnoremap("<leader>pp", ":echo expand('%:p')<CR>")
+nnoremap("<leader>pp", ":echo expand('%:p')<CR>")
 
 -- unbinding
-utils.nnoremap("<C-F>", "")
+nnoremap("<C-F>", "")
 ------------------------                  -------------------------
 ------------------------ Plugin Specifics -------------------------
 ------------------------                  -------------------------
 
 -- ranger
-utils.nnoremap("<C-n>", ":RnvimrToggle<CR>", silent)
+nnoremap("<C-n>", ":RnvimrToggle<CR>", silent)
 
 -- format code
-utils.nnoremap("<Leader>fm", ":Neoformat<CR>", silent)
+nnoremap("<Leader>fm", ":Neoformat<CR>", silent)
 
 -- Telescope
-utils.nnoremap("<C-p>", ":lua require('setup.telescope').find_files()<CR>")
-utils.nnoremap("<C-e>", ":lua require('setup.telescope').find_dir()<CR>", silent)
-utils.nnoremap("<leader>fw", ":Telescope live_grep<CR>", silent)
-utils.nnoremap("<leader>gc", ":Telescope git_commits<CR>", silent)
-utils.nnoremap("<leader>fb", ":Telescope buffers<CR>", silent)
-utils.nnoremap("<leader>fh", ":Telescope help_tags<CR>", silent)
-utils.nnoremap("<leader>rc", ":lua require'setup.telescope'.search_dotfiles()<CR>", silent)
-utils.nnoremap("<leader>fg", ":lua require'setup.telescope'.git_worktrees()<CR>", silent)
-utils.nnoremap("<leader>ct", ":lua require'setup.telescope'.create_git_worktree()<CR>", silent)
-utils.nnoremap("<leader>fy", ":lua require'setup.telescope'.neoclip()<CR>", silent)
-utils.nnoremap("<leader>ff", ":lua require'setup.telescope'.curbuf()<CR>", silent)
-utils.nnoremap("<leader>fc", ":Telescope commands<CR>", silent)
+nnoremap("<C-p>", ":lua require('setup.telescope').find_files()<CR>")
+nnoremap("<C-e>", ":lua require('setup.telescope').find_dir()<CR>", silent)
+nnoremap("<leader>fw", ":Telescope live_grep<CR>", silent)
+nnoremap("<leader>gc", ":Telescope git_commits<CR>", silent)
+nnoremap("<leader>fb", ":Telescope buffers<CR>", silent)
+nnoremap("<leader>fh", ":Telescope help_tags<CR>", silent)
+nnoremap("<leader>rc", ":lua require'setup.telescope'.search_dotfiles()<CR>", silent)
+nnoremap("<leader>fg", ":lua require'setup.telescope'.git_worktrees()<CR>", silent)
+nnoremap("<leader>ct", ":lua require'setup.telescope'.create_git_worktree()<CR>", silent)
+nnoremap("<leader>fy", ":lua require'setup.telescope'.neoclip()<CR>", silent)
+nnoremap("<leader>ff", ":lua require'setup.telescope'.curbuf()<CR>", silent)
+nnoremap("<leader>fc", ":Telescope commands<CR>", silent)
 
 -- Lsp
-utils.nnoremap("gD", ":lua vim.lsp.buf.declaration()<CR>", silent)
-utils.nnoremap("K", ":lua vim.lsp.buf.hover()<CR>", silent)
-utils.nnoremap("<C-k>", ":lua vim.lsp.buf.signature_help()<CR>", silent)
-utils.nnoremap("<leader>D", ":lua vim.lsp.buf.type_definition()<CR>", silent)
-utils.nnoremap("<leader>rn", ":lua vim.lsp.buf.rename()<CR>", silent)
-utils.nnoremap("<leader>d", ":lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", silent)
+nnoremap("gD", ":lua vim.lsp.buf.declaration()<CR>", silent)
+nnoremap("K", ":lua vim.lsp.buf.hover()<CR>", silent)
+nnoremap("<C-k>", ":lua vim.lsp.buf.signature_help()<CR>", silent)
+nnoremap("<leader>D", ":lua vim.lsp.buf.type_definition()<CR>", silent)
+nnoremap("<leader>rn", ":lua vim.lsp.buf.rename()<CR>", silent)
+nnoremap("<leader>d", ":lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", silent)
 -- Lsp Tele
-utils.nnoremap("gd", ":Telescope lsp_definitions<CR>", silent)
-utils.nnoremap("gr", ":Telescope lsp_references<CR>", silent)
-utils.nnoremap("<leader>ca", ":lua require('setup.telescope').lsp_code_actions()<CR>", silent)
-utils.nnoremap("<leader>gi", ":Telescope lsp_implementations<CR>", silent)
-utils.nnoremap("<leader>fs", ":lua require('setup.telescope').get_symbols()<CR>", silent)
-utils.nnoremap("<leader>tw", ":Telescope lsp_workspace_diagnostics<CR>", silent)
-utils.nnoremap("<leader>td", ":Telescope lsp_document_diagnostics<CR>", silent)
+nnoremap("gd", ":Telescope lsp_definitions<CR>", silent)
+nnoremap("gr", ":Telescope lsp_references<CR>", silent)
+nnoremap("<leader>ca", ":lua require('setup.telescope').lsp_code_actions()<CR>", silent)
+nnoremap("<leader>gi", ":Telescope lsp_implementations<CR>", silent)
+nnoremap("<leader>fs", ":lua require('setup.telescope').get_symbols()<CR>", silent)
+nnoremap("<leader>tw", ":Telescope lsp_workspace_diagnostics<CR>", silent)
+nnoremap("<leader>td", ":Telescope lsp_document_diagnostics<CR>", silent)
 
 -- Refactoring
-utils.nnoremap("<leader>rt", ":norm! V<CR> :lua require'setup.telescope'.refactor()<CR>", silent)
-utils.vnoremap("<leader>rt", ":lua require'setup.telescope'.refactor()<CR>", silent)
+nnoremap("<leader>rt", ":norm! V<CR> :lua require'setup.telescope'.refactor()<CR>", silent)
+vnoremap("<leader>rt", ":lua require'setup.telescope'.refactor()<CR>", silent)
 
 -- Harpoon
-utils.nnoremap("<leader>a", ":lua require'harpoon.mark'.add_file()<CR>", silent)
-utils.nnoremap("<leader>e", ":lua require'harpoon.ui'.toggle_quick_menu()<CR>", silent)
+nnoremap("<leader>a", ":lua require'harpoon.mark'.add_file()<CR>", silent)
+nnoremap("<leader>e", ":lua require'harpoon.ui'.toggle_quick_menu()<CR>", silent)
 
-utils.nnoremap("<leader>hn", ":lua require('harpoon.ui').nav_file(1)<CR>")
-utils.nnoremap("<leader>he", ":lua require('harpoon.ui').nav_file(2)<CR>")
-utils.nnoremap("<leader>ho", ":lua require('harpoon.ui').nav_file(3)<CR>")
-utils.nnoremap("<leader>hi", ":lua require('harpoon.ui').nav_file(4)<CR>")
+nnoremap("<leader>hn", ":lua require('harpoon.ui').nav_file(1)<CR>")
+nnoremap("<leader>he", ":lua require('harpoon.ui').nav_file(2)<CR>")
+nnoremap("<leader>ho", ":lua require('harpoon.ui').nav_file(3)<CR>")
+nnoremap("<leader>hi", ":lua require('harpoon.ui').nav_file(4)<CR>")
 
-utils.nnoremap("<leader>tn", ":lua require('harpoon.term').gotoTerminal(1)<CR>")
-utils.nnoremap("<leader>te", ":lua require('harpoon.term').gotoTerminal(2)<CR>")
+nnoremap("<leader>tn", ":lua require('harpoon.term').gotoTerminal(1)<CR>")
+nnoremap("<leader>te", ":lua require('harpoon.term').gotoTerminal(2)<CR>")
 
 -- fugitive
-utils.nnoremap("<leader>gs", ":Git<CR>", silent)
+nnoremap("<leader>gs", ":Git<CR>", silent)
 
 -- Todo Comment
-utils.nnoremap("<leader>ft", ":TodoTelescope<CR>", silent)
+nnoremap("<leader>ft", ":TodoTelescope<CR>", silent)
 
 -- Hop
-utils.nnoremap("<leader><leader>f", ":HopWord<CR>")
+nnoremap("<leader><leader>b", ":HopWordBC<CR>")
+nnoremap("<leader><leader>w", ":HopWordAC<CR>")
 
 -- undotree
-utils.nnoremap("<leader>u", ":UndotreeShow<CR>", silent)
+nnoremap("<leader>u", ":UndotreeShow<CR>", silent)
 
 -- treesitter-unit
 utils.xnoremap("iu", ":lua require('treesitter-unit').select()<CR>")
@@ -110,7 +113,7 @@ utils.onoremap("iu", ":<C-u>lua require('treesitter-unit').select()<CR>")
 utils.onoremap("au", ":<C-u>lua require('treesitter-unit').select(true)<CR>")
 
 -- symbols
-utils.nnoremap("<leader>ss", ":SymbolsOutline<CR>", silent)
+nnoremap("<leader>so", ":SymbolsOutline<CR>", silent)
 
 -- luasnips
 vim.cmd [[
@@ -121,6 +124,6 @@ vim.cmd [[
   snoremap <silent> <C-j> <Cmd>lua require('luasnip').jump(-1)<CR>
 ]]
 
-  utils.nnoremap("<leader><leader>d", ":DimmerToggle<CR>")
-  utils.nnoremap("<leader>od", ":lua dump(require('dimmer').get_state().overlays)<CR>")
-  utils.nnoremap("<leader>lw", ":lua dump(require('setup.dimmer').list_windows())<CR>")
+nnoremap("<leader><leader>d", ":DimmerToggle<CR>")
+nnoremap("<leader>od", ":lua dump(require('dimmer').get_state().overlays)<CR>")
+nnoremap("<leader>lw", ":lua dump(require('setup.dimmer').list_windows())<CR>")
