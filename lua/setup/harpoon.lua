34be9452e710e4e77b0e633843001b0b93e773ff
James Trew <j.trew10@gmail.com>
2022-02-15 21:06:05 -0500

    stub harpoon.utils.branch_key function

--- lua/setup/harpoon.lua
+++ lua/setup/harpoon.lua
@@ -1,9 +1,19 @@
 local has_harpoon, harpoon = pcall(require, "harpoon")
 if has_harpoon then
+  require("harpoon.utils").branch_key = function()
+    local branch = require("galaxyline.providers.vcs").get_git_branch()
+    if branch then
+        return vim.loop.cwd() .. "-" .. branch
+    else
+        return vim.loop.cwd()
+    end
+  end
+
   harpoon.setup({
     global_settings = {
       save_on_toggle = true,
       save_on_change = true,
+      mark_branch = true,
     },
   })
 end
