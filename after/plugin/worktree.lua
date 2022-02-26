3da7b92866c853c3367528416ac9349f41348bfa
James Trew <j.trew10@gmail.com>
2022-02-25 23:00:31 -0500

    plugin: change worktree-harpoon hooks

--- after/plugin/worktree.lua
+++ after/plugin/worktree.lua
@@ -5,12 +5,12 @@ if not has_worktree and not has_harpoon then
   return
 end
 
-local harpmark = require("harpoon.mark")
-local harpterm = require("harpoon.term")
-
-worktree.on_tree_change(function(op, _, _)
-  if op == worktree.Operations.Switch then
-    harpmark.clear_all()
-    harpterm.clear_all()
-  end
-end)
+-- local harpmark = require("harpoon.mark")
+-- local harpterm = require("harpoon.term")
+--
+-- worktree.on_tree_change(function(op, _, _)
+--   if op == worktree.Operations.Switch then
+--     harpmark.clear_all()
+--     harpterm.clear_all()
+--   end
+-- end)
