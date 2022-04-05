72cdd4d52a3a549ffe16f821863d409dcd2df661
James Trew <j.trew10@gmail.com>
2022-04-05 11:19:24 -0400

    move autocmds to its separate thing

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 Work = false
 local g = vim.g
-require("impatient").enable_profile()
-require("plugins")
-require("packer_compiled")
-require("options")
-
 g.mapleader = " "
 g.auto_save = false
 g.colors_name = "onedark"
 
+require("impatient").enable_profile()
+require("plugins")
+require("packer_compiled")
+require("options")
+require("autocmds")
 require("mappings")
---
+++ lua/autocmds.lua
@@ -0,0 +1,30 @@
+local utils = require("utils")
+
+vim.api.nvim_create_augroup("my_config", { clear = true })
+
+-- windows to close with "q"
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "help", "man", "lspinfo", "fugitive", "qf" },
+  command = "nnoremap <buffer><silent> q :quit<CR>",
+  group = "my_config",
+})
+
+-- get rid of weird formatoptions
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = "setlocal formatoptions-=r formatoptions-=o",
+  group = "my_config",
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  command = "silent! lua vim.highlight.on_yank()",
+  group = "my_config",
+})
+
+-- autoformat on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  command = ":%s/\\s\\+$//e",
+  group = "my_config",
+})
