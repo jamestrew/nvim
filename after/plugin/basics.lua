b56e8e72067a969bd71a4657c78792a270b9c028
James Trew <j.trew10@gmail.com>
2022-04-14 17:21:02 -0400

    fix: basic setup calling with blank config

---
+++ after/plugin/basics.lua
@@ -0,0 +1,23 @@
+local configs = {
+  ["fidget"] = { text = { spinner = { "🙈", "🙉", "🙊" } }, timer = { spinner_rate = 250 } },
+  ["nvim-autopairs"] = {},
+  ["colorizer"] = {},
+  ["gomove"] = {},
+  ["neoclip"] = {},
+  ["hop"] = { key = "tnhesoaiwfrudpclm" },
+  ["scrollbar"] = {},
+  ["nvim-tree"] = { hijack_netrw = false },
+}
+
+for plugin_name, config in pairs(configs) do
+  local ok, plugin = pcall(require, plugin_name)
+  if not ok then
+    vim.notify(string.format("Plugin %s is not available", plugin_name), vim.log.levels.WARN)
+  else
+    if vim.tbl_isempty(config) then
+      plugin.setup()
+    else
+      plugin.setup(config)
+    end
+  end
+end
