3413b5ddd9a6dcb79b35daa794cdeb522bb3ff71
James Trew <j.trew10@gmail.com>
2022-04-18 22:32:17 -0400

    tweaks

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,30 +1,30 @@
-vim.api.nvim_create_augroup("my_config", { clear = true })
+local my_group = vim.api.nvim_create_augroup("my_group", { clear = true })
 
 -- windows to close with "q"
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "help", "man", "lspinfo", "fugitive", "qf" },
   command = "nnoremap <buffer><silent> q :quit<CR>",
-  group = "my_config",
+  group = my_group,
 })
 
 -- get rid of weird formatoptions
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*",
   command = "setlocal formatoptions-=r formatoptions-=o",
-  group = "my_config",
+  group = my_group,
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
   command = "silent! lua vim.highlight.on_yank()",
-  group = "my_config",
+  group = my_group,
 })
 
 -- autoformat on save
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
   command = ":%s/\\s\\+$//e",
-  group = "my_config",
+  group = my_group,
 })
 
 -- TODO: is User autocmds doable with lua?
