0f5ae3b8a2d47594e9c4cb59838bf1312449f082
James Trew <j.trew10@gmail.com>
2022-04-27 20:20:27 -0400

    feat: switch over to vim.keymap.set

---
+++ after/plugin/luasnip.lua
@@ -0,0 +1,23 @@
+local ok, luasnip = pcall(require, "luasnip")
+
+if not ok then
+  vim.notify("luasnip not installed", vim.log.levels.WARN)
+end
+
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  if luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  end
+end)
+
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  if luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  end
+end)
+
+vim.keymap.set("i", "<C-l>", function()
+  if luasnip.choice_active() then
+    luasnip.change_choice(1)
+  end
+end)
