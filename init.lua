61efd10b1aa2d2558ee45e6b10b027c480eeccec
James Trew <j.trew10@gmail.com>
2022-07-30 16:19:37 -0400

    re-rename packer to plugins cuz conflicting names

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.g.auto_save = false
 vim.g.colors_name = "onedark"
 
 require("impatient").enable_profile()
-require("packer")
+require("plugins")
 require("packer_compiled")
 require("options")
 require("autocmds")
--- lua/packer_compiled.lua
+++ lua/packer_compiled.lua
@@ -404,7 +404,7 @@ vim.cmd [[au FileType markdown ++once lua require("packer.load")({'markdown-prev
 time([[Defining lazy-load filetype autocommands]], false)
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
-vim.cmd [[au BufRead * ++once lua require("packer.load")({'nvim-ts-context-commentstring', 'playground', 'undotree', 'emmet-vim', 'nvim-lsp-ts-utils'}, { event = "BufRead *" }, _G.packer_plugins)]]
+vim.cmd [[au BufRead * ++once lua require("packer.load")({'nvim-lsp-ts-utils', 'emmet-vim', 'playground', 'undotree', 'nvim-ts-context-commentstring'}, { event = "BufRead *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
 if should_profile then save_profiles() end
