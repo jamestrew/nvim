fde132cd811c12eebb1455757517aefe27de4d08
James Trew <j.trew10@gmail.com>
2022-09-17 13:50:13 -0400

    fix colorizer setup

--- after/plugin/basics.lua
+++ after/plugin/basics.lua
@@ -3,14 +3,14 @@ local import = require("utils").import
 local configs = {
   ["fidget"] = { text = { spinner = { "🙈", "🙉", "🙊" } }, timer = { spinner_rate = 250 } },
   ["nvim-autopairs"] = {},
-  ["colorizer"] = {},
+  ["colorizer"] = {"*"},
   ["gomove"] = {},
   ["neoclip"] = {},
   ["hop"] = { key = "tnhesoaiwfrudpclm" },
   ["scrollbar"] = {},
   ["nvim-surround"] = {},
   ["mason"] = {},
-  -- ["tint"] = { tint = -50 },
+  ["tint"] = { tint = -50 },
 }
 
 for plugin_name, config in pairs(configs) do
