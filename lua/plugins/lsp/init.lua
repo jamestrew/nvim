b8a7fe6740e1b1f9a45dc559c456b3a88f8c15e8
James Trew <j.trew10@gmail.com>
2023-09-10 14:07:12 -0400

    some rust debugging

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -92,6 +92,11 @@ M.config = function()
     lspconfig[server].setup(opts)
   end
 
+  -- rust
+  local extension_path = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension"
+  local codelldb_path = extension_path .. "/adapter/codelldb"
+  local liblldb_path = extension_path .. "/lldb/lib/liblldb.so"
+
   require("rust-tools").setup({
     tools = {
       inlay_hints = {
@@ -102,6 +107,9 @@ M.config = function()
     server = {
       on_attach = on_attach,
       capabilities = capabilities,
+    },
+    dap = {
+      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
     },
   })
 
