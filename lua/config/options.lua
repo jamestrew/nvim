83682a4666165d810a703562f34742c785e00b0f
James Trew <j.trew10@gmail.com>
2024-10-09 20:05:14 -0400

    add fancy treesitter foldtext

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -48,7 +48,6 @@ opt.wrap = false
 opt.colorcolumn = "90"
 
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-opt.foldtext = ""
 opt.foldmethod = "expr"
 opt.foldenable = true
 opt.foldlevel = 100
@@ -70,3 +69,40 @@ opt.joinspaces = false -- Two spaces and grade school, we're done
 
 -- set fillchars=eob:~
 opt.fillchars = { eob = "~" }
+
+local function fold_virt_text(result, s, lnum, coloff)
+  if not coloff then coloff = 0 end
+  local text = ""
+  local hl
+  for i = 1, #s do
+    local char = s:sub(i, i)
+    local hls = vim.treesitter.get_captures_at_pos(0, lnum, coloff + i - 1)
+    local _hl = hls[#hls]
+    if _hl then
+      local new_hl = "@" .. _hl.capture
+      if new_hl ~= hl then
+        table.insert(result, { text, hl })
+        text = ""
+        hl = nil
+      end
+      text = text .. char
+      hl = new_hl
+    else
+      text = text .. char
+    end
+  end
+  table.insert(result, { text, hl })
+end
+
+function _G.custom_foldtext()
+  local start = vim.fn.getline(vim.v.foldstart):gsub("\t", string.rep(" ", vim.o.tabstop))
+  local end_str = vim.fn.getline(vim.v.foldend)
+  local end_ = vim.trim(end_str)
+  local result = {}
+  fold_virt_text(result, start, vim.v.foldstart - 1)
+  table.insert(result, { " ... ", "Delimiter" })
+  fold_virt_text(result, end_, vim.v.foldend - 1, #(end_str:match("^(%s+)") or ""))
+  return result
+end
+
+vim.opt.foldtext = "v:lua.custom_foldtext()"
