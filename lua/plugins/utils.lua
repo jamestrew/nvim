77710437531ae0293c2ffc6a929351781ec457b7
James Trew <j.trew10@gmail.com>
2025-11-01 12:20:25 -0400

    re-enable lc

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -55,13 +55,8 @@ return {
     "kawre/leetcode.nvim",
     build = ":TSUpdate html",
     dependencies = {
-      "nvim-telescope/telescope.nvim",
       "nvim-lua/plenary.nvim", -- required by telescope
       "MunifTanjim/nui.nvim",
-
-      -- optional
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
     },
     lazy = "leetcode.nvim" ~= vim.fn.argv()[1],
     opts = {
@@ -78,8 +73,8 @@ return {
         },
       },
       storage = { home = "~/projects/leetcode" },
+      picker = { provider = "snacks-picker"}
     },
-    enabled = false,
   },
   {
     "iamcco/markdown-preview.nvim",
