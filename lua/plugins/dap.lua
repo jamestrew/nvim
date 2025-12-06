0d5eaac86277424ff8dde5d4fbf9ad8a9a5a0b3e
James Trew <j.trew10@gmail.com>
2025-12-05 22:57:17 -0500

    dap C support

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -53,6 +53,50 @@ function M.config()
     callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
   end
 
+  local dap = require("dap")
+  dap.adapters.gdb = {
+    type = "executable",
+    command = "gdb",
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+  }
+  local dap = require("dap")
+  dap.configurations.c = {
+    {
+      name = "Launch",
+      type = "gdb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      args = {}, -- provide arguments if needed
+      cwd = "${workspaceFolder}",
+      stopAtBeginningOfMainSubprogram = false,
+    },
+    {
+      name = "Select and attach to process",
+      type = "gdb",
+      request = "attach",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      pid = function()
+        local name = vim.fn.input("Executable name (filter): ")
+        return require("dap.utils").pick_process({ filter = name })
+      end,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      name = "Attach to gdbserver :1234",
+      type = "gdb",
+      request = "attach",
+      target = "localhost:1234",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+    },
+  }
+
   vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint" })
 
   require("dap-go").setup()
