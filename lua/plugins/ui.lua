91ec4332b077219116cd3473e84a06d03df3e2be
James Trew <j.trew10@gmail.com>
2026-05-03 00:29:20 -0400

    add nvim-origami

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -12,6 +12,17 @@ return {
     "Bekaboo/dropbar.nvim",
     opts = {},
   },
+  {
+    "chrisgrieser/nvim-origami",
+    event = "VeryLazy",
+    opts = {}, -- required even when using default config
+
+    -- recommended: disable vim's auto-folding
+    init = function()
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
+    end,
+  },
 
   {
     "lukas-reineke/indent-blankline.nvim",
